81531b60f121669931102fef00af8830897a0650
Dan Klco <dan.klco@sixdimensions.com>
2014-09-01 13:19:24 -0400

    Adding tunnels for mongo

--- create_prod_tunnels.sh
+++ create_prod_tunnels.sh
@@ -7,6 +7,8 @@ ssh -f root@192.168.24.20 -L 8101:localhost:8101 -N
 ssh -f root@192.168.24.20 -L 8102:localhost:8102 -N
 ssh -f root@192.168.24.20 -L 8103:localhost:8103 -N
 ssh -f root@192.168.24.20 -L 8104:localhost:8104 -N
+ssh -f root@192.168.24.20 -L 8108:localhost:8108 -N
+ssh -f root@192.168.24.20 -L 8109:localhost:8109 -N
 echo -e "Created the following SSH Tunnels:\nPort --- Host --- Remote Port\n"
 echo -e "8097 --- Author 1 --- 22"
 echo -e "8098 --- Author 1 --- 4502"
@@ -16,3 +18,5 @@ echo -e "8102 --- Publish 1 --- 4503"
 echo -e "8101 --- Publish 1 --- 22"
 echo -e "8104 --- Publish 2 --- 4503"
 echo -e "8103 --- Publish 2 --- 22"
+echo -e "8108 --- Mongo --- 22"
+echo -e "8109 --- Mongo --- 27017"
