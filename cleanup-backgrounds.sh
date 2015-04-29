82bf174851bcf7fef04367f217a52c74c171cea3
Dan Klco <dan.klco@sixdimensions.com>
2015-04-29 11:25:18 -0400

    Adding a script I use to cleanup the background images from IFTTT

---
+++ cleanup-backgrounds.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for image in /Users/dklco/Dropbox/IFTTT/hotredditwallpaper/*
+do
+	height=$(sips -g pixelHeight "$image" | tail -n1 | cut -d" " -f4)
+	width=$(sips -g pixelWidth "$image" | tail -n1 | cut -d" " -f4)
+	if [ "$height" = "" ] || [ "$width" = "" ]; then
+		echo "Removing: $image"
+		rm "$image"
+		continue
+	fi
+	if (("$height" < "900")) || (("$width" < "600")); then
+		echo "Removing: $image"
+		rm "$image"
+	fi
+	if (("$height" > "2880")) || (("$width" > "2880")); then
+		echo "Resizing: $image"
+		sips -Z 2880 "$image"
+	fi
+done
